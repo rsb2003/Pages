2665e508bcee84a5ca3c9d00376ca96a5cf5cd26
rsb2003 <ryan.belanger2@snhu.edu>
2023-09-18 15:08:56 -0400

    git

---
+++ pages/urls.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+__author__ = "Ryan Belanger"
+# pages/urls.py
+from django.urls import path
+from .views import HomePageView, AboutPageView # new
+urlpatterns = [
+ path("about/", AboutPageView.as_view(), name="about"), # new
+ path("", HomePageView.as_view(), name="home"),
+]
